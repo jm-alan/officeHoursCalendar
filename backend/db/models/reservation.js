81b1090a15d3c5e9a8b357b1654a571807ab8677
JM <j.maxwell.alan@gmail.com>
2021-09-23 19:31:54 -0400

    create reservation

---
+++ backend/db/models/reservation.js
@@ -0,0 +1,22 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, { DataTypes, fn }) => {
+  class Reservation extends Model {}
+
+  Reservation.init({
+    weekday: DataTypes.INTEGER,
+    studentName: DataTypes.STRING(50),
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: fn('now')
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: fn('now')
+    }
+  }, {
+    sequelize,
+    modelName: 'Reservation'
+  });
+};
